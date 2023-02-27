080250b327a234843afd93e03d410f33b6838b09
NikiHerci <nhercigon@foi.hr>
2023-02-27 20:11:29 +0100

    popravljena logika funkcionalnosti

--- javaScript.js
+++ javaScript.js
@@ -3,24 +3,60 @@ function eventListeners() {
     var inputPolje = document.getElementById('inputPolje');
     var button = document.getElementById('addButton');
     var gridElemenata = document.getElementById('grid');
+    var removeItems = document.getElementById('removeItems');
+    var items = [];
     button.addEventListener('click', function (event) {
         var unos = inputPolje.value;
-        if(unos!=""){
+        if (unos != "") {
+            if (items.includes(unos)) {
+                alert("Već ste unjeli ovu tehnologiju!")
+
+            } else {
+                items.push(unos);
+                
+                DodajElement();
+            }
+        } else {
+            event.preventDefault();
+        }
+    })
+    var removeItems = document.getElementById('removeItems');
+    removeItems.addEventListener('click', function (event) {
+        while (gridElemenata.childElementCount > 0) {
+            ObrisiElement();
+        }
+    })
+    inputPolje.addEventListener('keypress', function (event) {
+        var unos = inputPolje.value;
+        if (event.key == "Enter") {
+            if (unos != "") {
+                if (items.includes(unos)) {
+                    alert("Već ste unjeli ovu tehnologiju!")
+    
+                } else {
+                    items.push(unos);
+                    
+                    DodajElement();
+                }
+            } else {
+                event.preventDefault();
+            }
+        }
+    })
+    function DodajElement() {
+        var unos = inputPolje.value;
         var noviElement = document.createElement("p");
         noviElement.setAttribute("class", "tehnologije");
         noviElement.innerText = unos;
         gridElemenata.appendChild(noviElement);
-        inputPolje.value="";
-    }else{
-        event.preventDefault();
+        inputPolje.value = "";
+        removeItems.style.visibility = "visible";
     }
-    })
-    var removeItems=document.getElementById('removeItems');
-    removeItems.addEventListener('click',function(event){
-        while (gridElemenata.childElementCount>0) {
-            var firstChild = gridElemenata.firstChild;
-            gridElemenata.removeChild(firstChild);
-            inputPolje.value="";
-        } 
-    })
+    function ObrisiElement() {
+        var firstChild = gridElemenata.firstChild;
+        gridElemenata.removeChild(firstChild);
+        inputPolje.value = "";
+        removeItems.style.visibility = "hidden";
+        items=[]
+    }
 }
